bfd180dc0f55454c397501eb71cb05ce23020d92
Alex Dewhirst <alexanderdewhirst@Alexs-MacBook-Pro.local>
2020-02-20 15:49:07 -0500

    doc: Add ScheduleHelper method documentation test: Create ScheduleHelper RSpec tests Create a TestClass to extend the methods from the ScheduleHelper class to test the module methods.

--- helpers/schedule_helper.rb
+++ helpers/schedule_helper.rb
@@ -1,5 +1,9 @@
 module ScheduleHelper
+
     # Change hours to seconds for Time operations.
+    # Input:
+    # - Float
+    # * This value represents a duration of time in hours
     # Output:
     # - float
     def to_seconds(time)
--- spec/helpers/schedule_helper_spec.rb
+++ spec/helpers/schedule_helper_spec.rb
@@ -3,6 +3,27 @@ require_relative '../../helpers/schedule_helper'
 
 describe ScheduleHelper do
     
+    class TestClass
+    end
 
+    describe "#to_seconds" do
+        let(:time) { 4 }
+        let(:test_class) { TestClass.new }
+        before do
+            test_class.extend(ScheduleHelper)
+        end
+
+        context "should be a method" do
+            it { expect(test_class.methods).to include(:to_seconds) }
+        end
+
+        context "should return a float" do
+            it { expect(test_class.to_seconds(time)).to be_instance_of Float }
+        end
+
+        context "should convert hours to seconds" do
+            it { expect(test_class.to_seconds(time)).to eq 14400.0 }
+        end
+    end
 
 end
